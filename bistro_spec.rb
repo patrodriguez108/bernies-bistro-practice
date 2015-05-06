1db27b21814b41eb7cd3eafab9cefad4456212ba
Sam Blackman <samblackman@Maczor.local>
2015-05-05 21:40:56 -0400

    first commit

---
+++ bistro_spec.rb
@@ -0,0 +1,20 @@
+require_relative('bistro')
+
+describe Recipe do
+end
+
+describe Bistro do
+
+  it 'should load recipes' do
+  end
+
+  describe "#find_recipe_by_id" do
+    it 'should find a recipe by id' do
+    end
+
+    it "should raise an exception if it can't find a recipe" do
+      #How do I check exceptions? Here's an example
+      # expect { my_thing.method_that_raises }.to raise_error
+    end
+  end
+end
