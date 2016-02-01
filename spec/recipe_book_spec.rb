de63bd5bf6491b91d46bd70f9fdf3d87416e7aee
Torey Hickman <torey@toreyhickman.com>
2016-02-01 13:54:00 -0500

    Remove should from test descriptions

--- spec/recipe_book_spec.rb
+++ spec/recipe_book_spec.rb
@@ -3,10 +3,10 @@ require_relative "../recipe_book"
 describe RecipeBook do
 
   describe "#find_recipe_by_id" do
-    xit 'should find a recipe by id' do
+    xit 'finds a recipe by id' do
     end
 
-    xit "should raise an exception if it can't find a recipe" do
+    xit "raises an exception if it can't find a recipe" do
       #How do I check exceptions? Here's an example
       # expect { my_thing.method_that_raises }.to raise_error
     end
