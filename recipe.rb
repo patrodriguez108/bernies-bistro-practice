8274023dc53e9abf895a189c62936ee6be277dd5
Patrick <pat.rodriguez108@gmail.com>
2018-02-15 13:50:45 -0600

    Add initialize method to recipe class

--- recipe.rb
+++ recipe.rb
@@ -1,4 +1,11 @@
 class Recipe
-  # I need to finish this before I give Bernie the program...
-
+	attr_reader :id, :name, :description, :ingredients, :directions
+  
+  def initialize(args = {})
+  	@id = args.fetch(:id, 0)
+  	@name = args.fetch(:name, '')
+  	@description = args.fetch(:description, '')
+  	@ingredients = args.fetch(:ingredients, '')
+  	@directions = args.fetch(:directions, '')
+  end
 end
